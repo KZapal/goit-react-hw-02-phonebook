703f1812bfa1315c26871cec4fa342b108f54a85
KZapal <kamil.zapal@o2.pl>
2024-01-20 14:29:12 +0100

    0 styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return <div>dziala?</div>;
+  }
+}
+
+export default App;
